bdf89fba373024428eaaf0364a556ffb60405cc4
lautaro gatti <lautarogatti98@gmail.com>
2024-04-25 01:17:37 -0300

    subida a repo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calculadoraDeTiempo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int d, v, t;
+            Console.WriteLine("Bienvenido a la calculadora de tiempo de viajes, por favor, inserte la cantidad de kilometros a recorrer:");
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine("Por favor, ingrese la velocidad elegida para el viaje:");
+            v = int.Parse(Console.ReadLine());
+            t = d / v;
+            Console.WriteLine("El tiempo aproximado de el viaje es de: " + t + " horas");
+            Console.WriteLine("Gracias por utilizar la calculadora de tiempo de viajes");
+        }
+    }
+}
